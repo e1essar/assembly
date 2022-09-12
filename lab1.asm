a23b49fe2075d6155a217b1b87d5d479c54a0905
e1essar <80064778+e1essar@users.noreply.github.com>
2022-09-12 16:55:02 +0700

    Update lab1.asm

--- lab1.asm
+++ lab1.asm
@@ -18,36 +18,45 @@ mul ebx ;eax = eax*ebx
 
 mov [count], eax
 
-lea edi, [s]
-xor ecx, ecx
-mov ebx, 10
-div:
-xor edx, edx
-inc ecx
-div ebx
-add edx, '0'
-push edx
-test eax, eax
-jnz div
-
-mov [lenn], ecx
-store:
-pop eax
-stosb
-loop store
-
 mov edx, len
 mov ecx, msg
 mov ebx, 1
 mov eax, 4
 int 0x80
 
-mov eax, 4 ; вывожу строку
+mov eax, [count]
+xor ebx, ebx
+xor ecx,ecx
+xor edx,edx
+    
+mov ebx, 10;  
+ 
+continueDIV:
+ 
+div  ebx; деление нашего числа на 10 
+push edx; помещение остатка в стек(последней цифры числа)
+inc  ecx; повышение счетчика ecx(количества цифр)
+ 
+xor edx, edx ; обнуляем регистр
+ 
+cmp  eax, 0; проверка целой части числа(== 0?)
+jne continueDIV; если не равно 0 то цикл
+ 
+fromStek:
+pop edx;
+mov [n]   , edx
+add [n]   , byte 48
+mov [buff], ecx ; запоминаем текущее количество итераций, так как , далее выводим символ и необходимо использовать регистр ecx
+ 
+mov edx, 1  ; вывод остатков(числа)
+mov ecx, n
 mov ebx, 1
-mov ecx, s
-mov edx, [lenn]
-
+mov eax, 4
 int 0x80
+
+mov ecx, [buff]  ; помещение ecx в переменную для сохранения значения 
+ 
+loop fromStek    ; цикл
 
 xor eax, eax
 xor ebx, ebx
@@ -59,30 +68,16 @@ mov ebx, [B]
 mov ecx, [C]
 
 mul ebx
+xor edx, edx
 xchg eax, ebx
 mov eax, [count]
 mul ecx
+xor edx, edx
 mul ecx
+xor edx, edx
 xchg eax, ebx
 sub eax, ebx
-
-lea edi, [s1]
-xor ecx, ecx
-mov ebx, 10
-div1:
-xor edx, edx
-inc ecx
-div ebx
-add edx, '0'
-push edx
-test eax, eax
-jnz div1
-
-mov [lenn1], ecx
-store1:
-pop eax
-stosb
-loop store1
+mov [count], eax
 
 mov edx, len1
 mov ecx, msg1
@@ -90,28 +85,53 @@ mov ebx, 1
 mov eax, 4
 int 0x80
 
+mov eax, [count]
 
-mov eax, 4 ; вывожу строку
+xor ebx, ebx
+xor ecx,ecx
+xor edx,edx
+    
+mov ebx, 10;  
+ 
+continueDIV1:
+ 
+div  ebx; деление нашего числа на 10 
+push edx; помещение остатка в стек(последней цифры числа)
+inc  ecx; повышение счетчика ecx(количества цифр)
+ 
+xor edx, edx ; обнуляем регистр
+ 
+cmp  eax, 0; проверка целой части числа(== 0?)
+jne continueDIV1; если не равно 0 то цикл
+ 
+fromStek1:
+pop edx;
+mov [n]   , edx
+add [n]   , byte 48
+mov [buff], ecx ; запоминаем текущее количество итераций, так как , далее выводим символ и необходимо использовать регистр ecx
+ 
+mov edx, 1  ; вывод остатков(числа)
+mov ecx, n
 mov ebx, 1
-mov ecx, s1
-mov edx, [lenn1]
+mov eax, 4
 int 0x80
+
+mov ecx, [buff]  ; помещение ecx в переменную для сохранения значения 
+ 
+loop fromStek1    ; цикл
+
 
 mov eax, 1
 int 0x80
 
 section .data
-A dd 11
-B dd 10
-C dd 4
+A dd 65
+B dd 23
+C dd 14
 count dd 0
+n dd 0
+buff dd 0
 msg db "count: "
 len equ $ - msg
 msg1 db " area: "
 len1 equ $ - msg1
-
-section .bss
-s resb 255
-lenn resd 1
-s1 resb 255
-lenn1 resd 1
