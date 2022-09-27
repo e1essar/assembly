313ea38e449796cadde04bc9b9a1f6ee3ed5ab5f
e1essar <80064778+e1essar@users.noreply.github.com>
2022-09-28 01:17:00 +0700

    Update lab2.asm

--- lab2.asm
+++ lab2.asm
@@ -4,50 +4,54 @@ global _start
 
 _start:
 
-    mov eax, [x] 
-    cmp eax, 0
-    jle L1
-    ;if (x>0 && y>0) NO else L1
-    mov eax, [y] 
-    cmp eax, 0
-    jle L1
+    mov eax, [x] ; eax = x
+    mov ebx, [y] ; ebx = y
+   
+    ;if (x>0 && y>0) NO else L1 (elimination of 1st quarter)
+    cmp eax, 0 ; x > 0 or !(x <= 0)
+    jle L1     ; if not goto L1
     
-    jmp NO
+    cmp ebx, 0 ; y > 0 or !(y <= 0)
+    jle L1     ; if not goto L1
     
-    L1:
-    mov eax, [x]
-    imul eax
-    mov ebx, eax
-    mov eax, [y]
-    imul eax
-    add eax, ebx
+    jmp NO     ; if goto NO
     
-    cmp eax, 4 ;if (x*x+y*y < 4) NO else L2
-    jge L2
+    L1: 
+    ;if (x*x+y*y < 4) NO else L2 (in-circle elimination)
+    mov eax, [x] ; eax = x
+    mul eax      ; eax = x*x
+    mov ebx, eax ; ebx = x*x
+    mov eax, [y] ; eax = y
+    mul eax      ; eax = y*y
+    add eax, ebx ; eax = x*x+y*y
     
-    jmp NO
-    
+    cmp eax, 4 ; x*x+y*y < 4
+    jge L2     ; if not goto L2
+       
+    jmp NO     ; if goto NO
+     
     L2:
-    mov eax, [x]
-    cmp eax, 2 ;if
-    jg L3
+    ;if (x <= 2 && x >= (-2) && y <= 2 && y >= (-2)) (in-square inclusion)
+    mov eax, [x] ; eax = x
+    cmp eax, 2   ; x <= 2
+    jg L3        ; if not goto L3
     
-    cmp eax, -2
-    jl L3
+    cmp eax, -2  ; x >= 2
+    jl L3        ; if not goto L3
     
-    mov eax, [y]
-    cmp eax, 2
-    jg L3
+    mov eax, [y] ; eax = y
+    cmp eax, 2   ; y <= 2
+    jg L3        ; if not goto L3
     
-    cmp eax, -2
-    jl L3
+    cmp eax, -2  ; y >= -2
+    jl L3        ; if not goto L3
     
-    jmp YES
+    jmp YES      ; if goto YES
     
     L3:
-    jmp NO
+    jmp NO       ; goto NO
     
-    YES:
+    YES:         ; message that point is in the area
     
     mov     ecx, msgYes
     mov     edx, lenYes
@@ -55,9 +59,9 @@ _start:
     mov     eax, 4
     int     0x80
     
-    jmp exit
+    jmp exit     ; goto exit
     
-    NO:
+    NO:          ; message that point in not in the area
     
     mov     ecx, msgNo
     mov     edx, lenNo
@@ -71,9 +75,9 @@ _start:
     int     0x80
 
 section .data
-    x dd 3
-    y dd 0
-    msgYes db  "Yes"
+    x dd 2
+    y dd 2
+    msgYes db  "Yes, the point is on the area"
     lenYes equ $ - msgYes
-    msgNo db  "No"
+    msgNo db  "No, the point is not in the area"
     lenNo equ $ - msgNo
