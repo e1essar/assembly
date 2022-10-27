962c5db415bae2b379f39a94c37ce84e28879394
e1essar <igorvaliulin770@gmail.com>
2022-10-28 00:52:32 +0700

    test

--- test.cpp
+++ test.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main() {
-    int sign;
-    int count = 0;
-    int sum = 0; 
-    int n = 1234;
-    int n1 = n;
+    int n = 55;
+    int x, y, z;
 
-    while(n1!=0) {
-        count++;
-        n1 = n1/10;
-    }
-
-    if(count%2 == 0) {
-        sign = -1;
-    }
-    else {
-        sign = 1;
-    }
-
-    while(n!=0) {
-        sum = sum + (n%10)*sign;
-        sign = sign * (-1);
-        n = n / 10;
-    }
-    cout << sum;
+    for(x=0;x<n;x++)
+        for(y=0;y<n;y++)
+            for(z=0;z<n;z++)
+                if((x*x+y*y+z*z) == n) {
+                    cout << x << ' ' << y << ' ' << z;
+                    return 1;
+                }
+    cout << "false";            
+    return 0;
 }
